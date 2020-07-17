8dbe439f1b8987102f4122f5667b295d11debd7d
Kyle Andrews <codingwithkyle@gmail.com>
2020-07-17 09:04:31 -0400

    Updated lighthouse widget UX

--- src/assetbundles/lightkeeper/dist/js/widget.js
+++ src/assetbundles/lightkeeper/dist/js/widget.js
@@ -25,49 +25,59 @@ async function runAudit(e){
     });
     const response = await request.json();
     if (request.ok){
-        const maxOffset = 251;
-
-        performanceNumber.innerText = response.performance * 100;
-        performanceLine.style.strokeDashoffset = maxOffset - maxOffset * response.performance;
-        if (response.performance <= 0.49){
-            performanceLine.setAttribute('level', 'fail');
-        }else if (response.performance < 0.9){
-            performanceLine.setAttribute('level', 'warn');
-        }else{
-            performanceLine.setAttribute('level', 'pass');
-        }
-
-        accessibilityeNumber.innerText = response.accessibility * 100;
-        accessibilityLine.style.strokeDashoffset = maxOffset - maxOffset * response.accessibility;
-        if (response.accessibility <= 0.49){
-            accessibilityLine.setAttribute('level', 'fail');
-        }else if (response.accessibility < 0.9){
-            accessibilityLine.setAttribute('level', 'warn');
-        }else{
-            accessibilityLine.setAttribute('level', 'pass');
-        }
-
-        bestPracticesNumber.innerText = response.bestPractices * 100;
-        bestPracticesLine.style.strokeDashoffset = maxOffset - maxOffset * response.bestPractices;
-        if (response.bestPractices <= 0.49){
-            bestPracticesLine.setAttribute('level', 'fail');
-        }else if (response.bestPractices < 0.9){
-            bestPracticesLine.setAttribute('level', 'warn');
-        }else{
-            bestPracticesLine.setAttribute('level', 'pass');
-        }
-
-        seoNumber.innerText = response.seo * 100;
-        seoLine.style.strokeDashoffset = maxOffset - maxOffset * response.seo;
-        if (response.seo <= 0.49){
-            seoLine.setAttribute('level', 'fail');
-        }else if (response.seo < 0.9){
-            seoLine.setAttribute('level', 'warn');
-        }else{
-            seoLine.setAttribute('level', 'pass');
-        }
-
         widget.setAttribute('state', 'idling');
+
+        const maxOffset = 251;
+        performanceLine.style.strokeDashoffset = maxOffset;
+        accessibilityLine.style.strokeDashoffset = maxOffset;
+        bestPracticesLine.style.strokeDashoffset = maxOffset;
+        seoLine.style.strokeDashoffset = maxOffset;
+        performanceLine.setAttribute('level', 'neutral');
+        accessibilityLine.setAttribute('level', 'neutral');
+        bestPracticesLine.setAttribute('level', 'neutral');
+        seoLine.setAttribute('level', 'neutral');
+
+        setTimeout(()=>{
+            performanceNumber.innerText = response.performance * 100;
+            performanceLine.style.strokeDashoffset = maxOffset - maxOffset * response.performance;
+            if (response.performance <= 0.49){
+                performanceLine.setAttribute('level', 'fail');
+            }else if (response.performance < 0.9){
+                performanceLine.setAttribute('level', 'warn');
+            }else{
+                performanceLine.setAttribute('level', 'pass');
+            }
+
+            accessibilityeNumber.innerText = response.accessibility * 100;
+            accessibilityLine.style.strokeDashoffset = maxOffset - maxOffset * response.accessibility;
+            if (response.accessibility <= 0.49){
+                accessibilityLine.setAttribute('level', 'fail');
+            }else if (response.accessibility < 0.9){
+                accessibilityLine.setAttribute('level', 'warn');
+            }else{
+                accessibilityLine.setAttribute('level', 'pass');
+            }
+
+            bestPracticesNumber.innerText = response.bestPractices * 100;
+            bestPracticesLine.style.strokeDashoffset = maxOffset - maxOffset * response.bestPractices;
+            if (response.bestPractices <= 0.49){
+                bestPracticesLine.setAttribute('level', 'fail');
+            }else if (response.bestPractices < 0.9){
+                bestPracticesLine.setAttribute('level', 'warn');
+            }else{
+                bestPracticesLine.setAttribute('level', 'pass');
+            }
+
+            seoNumber.innerText = response.seo * 100;
+            seoLine.style.strokeDashoffset = maxOffset - maxOffset * response.seo;
+            if (response.seo <= 0.49){
+                seoLine.setAttribute('level', 'fail');
+            }else if (response.seo < 0.9){
+                seoLine.setAttribute('level', 'warn');
+            }else{
+                seoLine.setAttribute('level', 'pass');
+            }
+        }, 150);
     }else{
         widget.setAttribute('state', 'error');
         console.log(response);
